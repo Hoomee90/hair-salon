2dffaf98ed6462f256ee2a2f7c9a7085516caad9
Samantha Callie <Hoomee90@gmail.com>
2024-06-25 14:29:40 -0700

    add properties to stylist and client models

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -5,6 +5,9 @@ namespace HairSalon.Models
 	public class Stylist
 	{
 		public int StylistId { get; set; }
+		public string Name { get; set; }
+		public string Specialty { get; set; }
+		public int YearsWorked { get; set; }
 		public List<Client> Clients { get; set; }
 	}
 }
