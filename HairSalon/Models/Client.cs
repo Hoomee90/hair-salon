57e744ccc11f5e00141302539478f9148a9f6d75
Samantha Callie <Hoomee90@gmail.com>
2024-06-25 14:09:58 -0700

    create basic model outlines

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,9 @@
+namespace HairSalon.Models
+{
+	public class Client
+	{
+		public int ClientId { get; set; }
+		public int StylistId { get; set; }
+		public Stylist Stylist { get; set; }
+	}
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+	public class Stylist
+	{
+		public int StylistId { get; set; }
+		public List<Client> Clients { get; set; }
+	}
+}
